fad6a5b284f3282dbe54da199a66a5fd3b56529a
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-03-15 13:16:53 +0530

    added singup page

--- app/src/main/java/com/example/trolly/MainActivity.java
+++ app/src/main/java/com/example/trolly/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.trolly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button SignupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SignupButton=(Button) findViewById(R.id.SignupButton);
+
+        SignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignupPage();
+            }
+        });
+
+        }
+        public void openSignupPage(){
+            Intent intent= new Intent(this, SignUpPage.class);
+            startActivity(intent);
     }
 }
