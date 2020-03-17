3931b1eecdcffe8372cd9cc15ca1b6b0185e0be2
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-03-17 16:54:42 +0530

    Retrieved profile data from the databse to Profile details activity

--- app/src/main/java/com/example/authenticatorapp/MainActivity.java
+++ app/src/main/java/com/example/authenticatorapp/MainActivity.java
@@ -5,20 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button mProfileBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mProfileBtn = findViewById(R.id.profileBtn);
+
+        mProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Profile.class));
+            }
+        });
+
+
     }
 
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
         startActivity(new Intent(getApplicationContext(),Login.class ));
         finish();
+
+
     }
 }
