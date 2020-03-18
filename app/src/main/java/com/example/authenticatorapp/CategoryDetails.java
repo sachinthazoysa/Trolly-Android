e96bd06ed74d70801d4c37665158106bad556791
MenuraDewalegama <61499065+MenuraDewalegama@users.noreply.github.com>
2020-03-18 16:05:32 +0530

    Category details page are now functioning. Changes applied to activity_main.xml. (There were some buttons which not created for this page)

--- app/src/main/java/com/example/authenticatorapp/CategoryDetails.java
+++ app/src/main/java/com/example/authenticatorapp/CategoryDetails.java
@@ -3,12 +3,24 @@ package com.example.authenticatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CategoryDetails extends AppCompatActivity {
+
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_details);
+
+        backButton = findViewById(R.id.backBtn);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
