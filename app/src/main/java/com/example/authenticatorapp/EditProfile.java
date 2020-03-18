2ff58d28a3a6e718513645db8eaf17ddfb795935
sachinthazoysa <55564245+sachinthazoysa@users.noreply.github.com>
2020-03-18 13:10:19 +0530

    Made some desings in edit profile activity

--- app/src/main/java/com/example/authenticatorapp/EditProfile.java
+++ app/src/main/java/com/example/authenticatorapp/EditProfile.java
@@ -8,10 +8,22 @@ import android.view.View;
 import android.widget.Button;
 
 public class EditProfile extends AppCompatActivity {
-
+    Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        submit=findViewById(R.id.SubmitEdit);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Profile.class));
+
+            }
+        });
+
     }
+
+
 }
